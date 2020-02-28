066afee33595f4b55b7430bc26ca25acb58315d0
Vlad1111js <vladchumak181@gmail.com>
2020-02-28 02:59:44 +0200

    commit

---
+++ dom1.1.js
@@ -0,0 +1,53 @@
+// Задания №1
+//let name = prompt('Укажите пожалуйста свое имя!');
+//alert('Привет, ' + name);
+
+// Задания №2
+//let yOB = prompt('Укажите год рождения')
+//const year = 2020;
+//let result = year - yOB;
+//alert('Ваш возраст: ' + result);
+
+// Задания №3
+//let length = prompt('Введите длину стороны квадрата')
+//let result = 4* length;
+//alert('Периметр квадрата : ' + result);
+
+// Задания №4
+//let radius = prompt('Введите радиус окружности')
+//let result = Math.PI*Math.pow(radius, 2);
+//alert('Площадь окружности : ' + result);
+
+// Задания №5
+//let distance = prompt('Укажите расстояние в км между двумя городами') 
+//let time = prompt('За сколько часов хотите добраться')
+//let result = distance / time;
+//alert('Евро : ' + result);
+
+// Задания №6
+//let usd = prompt('Укажите сумму в долларах') 
+//const EUR_RATE = 0.91;
+//const result = EUR_RATE * usd;
+//alert('Сумма в eur - ' + result);
+
+// Задания №7
+//let gigabyte = prompt('Укажите объем флешки в ГБ') 
+//const files = 820;
+//const result = gigabyte * 1000 / files;
+//alert('Файлы на флешке - ' + Math.floor(result));
+
+// Задания №8
+//let sumMoney = +prompt('Сумма денег') 
+//let priceOneChocolate = +prompt('Цена шоколадки')
+//const amountChocolate = Math.floor(sumMoney / priceOneChocolate);
+//const balance = sumMoney - amountChocolate * priceOneChocolate;
+//alert(amountChocolate);
+//alert(balance);
+
+// Задания №9
+//let nValue = prompt('Укажите трехзначное число!');
+//let n1 = nValue % 10;
+//nValue = (nValue - n1) / 10;
+//let n2 = nValue % 10;
+//let n3 = (nValue - n2) / 10;
+//alert(String(n1) + n2 + n3);
